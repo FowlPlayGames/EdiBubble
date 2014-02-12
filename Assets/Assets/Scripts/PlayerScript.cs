5e61400ff29c2e67d4fe61588a3bff44f8590884
JG24Drive45 <JG24Drive45@gmail.com>
2014-02-11 21:56:53 -0500

    Collision & Rebounding
    Player collision with rock walls and rebounding movement off of the rock
    walls

--- Assets/Assets/Scripts/PlayerScript.cs
+++ Assets/Assets/Scripts/PlayerScript.cs
@@ -8,7 +8,7 @@ public class PlayerScript : MonoBehaviour {
 	private const float fMinSize = 1.0f;								// Minimum size player bubble can be
 	private const float fMaxSize = 10.0f;								// Maximum size player bubble can be
 	private const float fSizeIncrement = 1.0f;							// How much to grow player bubble by
-	private const float fOriginalReboundTime = 2.0f;
+	private const float fOriginalReboundTime = 0.25f;
 
 	// PUBLIC VARS
 	public float fSpeed;												// Speed of the player bubble
@@ -19,7 +19,7 @@ public class PlayerScript : MonoBehaviour {
 	// PRIVATE VARS
 	private Vector3 v3Tilt;												// Vector to hold tilt values
 	private bool bRebounding;											// Did the player recently collide with rocks?
-	private Vector3 v3ReboundVec;
+	private Vector3 v3ReboundVec;										// Rebounding velocity
 
 
 
@@ -34,7 +34,7 @@ public class PlayerScript : MonoBehaviour {
 
 	void Update () 
 	{
-		if( !bRebounding )
+		if( !bRebounding )												// If not already rebounding from a collision with wall
 		{
 			v3Tilt.x = Input.acceleration.x * fSpeed * Time.deltaTime;	// Calculate tilt value in x direction
 			v3Tilt.y = ( Input.acceleration.y + fYTiltCalibration ) * 	// Calculate tilt value in y direction and account for calibration
@@ -44,14 +44,15 @@ public class PlayerScript : MonoBehaviour {
 		}
 		else
 		{
-			transform.position += ( v3ReboundVec * fSpeed * Time.deltaTime );
-			fReboundTimer -= Time.deltaTime;
+			transform.position += ( v3ReboundVec * fSpeed * 			// Update the position by the rebound vector
+			                         Time.deltaTime );
+			fReboundTimer -= Time.deltaTime;							// Decrement the rebound timer
 		}
 
-		if( fReboundTimer <= 0.0f )
+		if( fReboundTimer <= 0.0f )										// Check to see if the rebound timer has expired
 		{
-			fReboundTimer = fOriginalReboundTime;
-			bRebounding = false;
+			fReboundTimer = fOriginalReboundTime;						// Reset the rebound timer for the next collision with a wall
+			bRebounding = false;										// Set fRebounding to false so that position can be updated via input
 		}
 
 		// TODO -- CLIP THE BOUNDS OF THE BUBBLE
@@ -60,6 +61,7 @@ public class PlayerScript : MonoBehaviour {
 	void OnCollisionEnter2D( Collision2D other )
 	{
 		// TODO -- NEED TO WRITE THIS SHIT
+		// IF YOU ATE A SMALLER BUBBLE
 		if( other.gameObject.tag == "TestCube" )
 		{
 			Debug.Log( "You hit shit!" );
@@ -76,29 +78,35 @@ public class PlayerScript : MonoBehaviour {
 				transform.localScale += new Vector3( 1.0f, 1.0f, 0.0f );// This will need to be moved to the growbubble function
 			}
 		}
-		else if( other.gameObject.tag == "Rocks" )
+		// IF YOU COLLIDED WITH ROCKS ON THE LEFT OR THE RIGHT
+		else if( other.gameObject.tag == "LeftRocks" || other.gameObject.tag == "RightRocks" )
 		{
-			Debug.Log( "Hit a rock" );
-
+			Debug.Log( "Hit the left or right rocks" );
 			Vector3 temp = Vector3.zero;
 			temp.x = Input.acceleration.x;								// Calculate tilt value in x direction
-			temp.y = ( Input.acceleration.y + fYTiltCalibration );		// Calculate tilt value in y direction and account for calibration
+			temp.y = Input.acceleration.y + fYTiltCalibration;			// Calculate tilt value in y direction and account for calibration
 
-			v3ReboundVec = Rebound( fReboundTimer, temp );
+			temp.x *= -1.0f;
+			v3ReboundVec = temp;
+			Rebound( fReboundTimer );
+		}
+		else if( other.gameObject.tag == "TopRocks" || other.gameObject.tag == "BottomRocks" )
+		{
+			Debug.Log( "Hit the top or bottom rocks" );
+			Vector3 temp = Vector3.zero;
+			temp.x = Input.acceleration.x;
+			temp.y = Input.acceleration.y + fYTiltCalibration;
 
-			if( transform.position.x <= other.gameObject.transform.position.x )
-			{
-				transform.position += new Vector3( 0.01f, 0.0f, 0.0f );
-			}
+			temp.y *= -1.0f;
+			v3ReboundVec = temp;
+			Rebound( fReboundTimer );
 		}
 	}
 
-	public Vector3 Rebound( float timer, Vector3 tempTilt )
+	public void Rebound( float timer )
 	{
 		bRebounding = true;
 		fReboundTimer = timer;
-
-		return tempTilt * -1.0f;
 	}
 
 	public void GrowBubble( float currSize, float desiredSize )
