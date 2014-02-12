1ab0e86e2da56347f8156390a68ff4e48192eb77
JG24Drive45 <JG24Drive45@gmail.com>
2014-02-11 19:46:11 -0500

    Broke
    Stuff -- Matt's fault

--- Assets/Assets/Scripts/PlayerScript.cs
+++ Assets/Assets/Scripts/PlayerScript.cs
@@ -8,30 +8,51 @@ public class PlayerScript : MonoBehaviour {
 	private const float fMinSize = 1.0f;								// Minimum size player bubble can be
 	private const float fMaxSize = 10.0f;								// Maximum size player bubble can be
 	private const float fSizeIncrement = 1.0f;							// How much to grow player bubble by
+	private const float fOriginalReboundTime = 2.0f;
 
 	// PUBLIC VARS
 	public float fSpeed;												// Speed of the player bubble
 	public float fCurrentSize;											// Current size of the player bubble
 	public float fMaxGrowSize;											// Maximum size player bubble can grow to
+	public float fReboundTimer;											// Timer to turn off rebounding
 
 	// PRIVATE VARS
 	private Vector3 v3Tilt;												// Vector to hold tilt values
+	private bool bRebounding;											// Did the player recently collide with rocks?
+	private Vector3 v3ReboundVec;
+
 
 
 	void Start () 
 	{
-		v3Tilt = Vector3.zero;											// Zero out v3Tilt vector
-		fCurrentSize = 1.0f;											// Player will start at size 1.0f
-		fMaxGrowSize = 1.0f; 											// Player currently cannot grow past 1.0f
+		v3Tilt 			= Vector3.zero;									// Zero out v3Tilt vector
+		fCurrentSize 	= 1.0f;											// Player will start at size 1.0f
+		fMaxGrowSize 	= 1.0f; 										// Player currently cannot grow past 1.0f
+		fReboundTimer	= fOriginalReboundTime;							// Set the rebound timer
+		bRebounding 	= false;										// Did the player recently collide with rocks?
 	}
 
 	void Update () 
 	{
-		v3Tilt.x = Input.acceleration.x * fSpeed * Time.deltaTime;		// Calculate tilt value in x direction
-		v3Tilt.y = ( Input.acceleration.y + fYTiltCalibration ) * 		// Calculate tilt value in y direction and account for calibration
-			fSpeed * Time.deltaTime;
+		if( !bRebounding )
+		{
+			v3Tilt.x = Input.acceleration.x * fSpeed * Time.deltaTime;	// Calculate tilt value in x direction
+			v3Tilt.y = ( Input.acceleration.y + fYTiltCalibration ) * 	// Calculate tilt value in y direction and account for calibration
+				fSpeed * Time.deltaTime;
 
-		transform.position += v3Tilt;									// Update the overall position
+			transform.position += v3Tilt;								// Update the overall position
+		}
+		else
+		{
+			transform.position += ( v3ReboundVec * fSpeed * Time.deltaTime );
+			fReboundTimer -= Time.deltaTime;
+		}
+
+		if( fReboundTimer <= 0.0f )
+		{
+			fReboundTimer = fOriginalReboundTime;
+			bRebounding = false;
+		}
 
 		// TODO -- CLIP THE BOUNDS OF THE BUBBLE
 	}
@@ -55,9 +76,32 @@ public class PlayerScript : MonoBehaviour {
 				transform.localScale += new Vector3( 1.0f, 1.0f, 0.0f );// This will need to be moved to the growbubble function
 			}
 		}
+		else if( other.gameObject.tag == "Rocks" )
+		{
+			Debug.Log( "Hit a rock" );
+
+			Vector3 temp = Vector3.zero;
+			temp.x = Input.acceleration.x;								// Calculate tilt value in x direction
+			temp.y = ( Input.acceleration.y + fYTiltCalibration );		// Calculate tilt value in y direction and account for calibration
+
+			v3ReboundVec = Rebound( fReboundTimer, temp );
+
+			if( transform.position.x <= other.gameObject.transform.position.x )
+			{
+				transform.position += new Vector3( 0.01f, 0.0f, 0.0f );
+			}
+		}
 	}
 
-	public void GrowBubble()
+	public Vector3 Rebound( float timer, Vector3 tempTilt )
+	{
+		bRebounding = true;
+		fReboundTimer = timer;
+
+		return tempTilt * -1.0f;
+	}
+
+	public void GrowBubble( float currSize, float desiredSize )
 	{
 		// TODO -- NEED TO WRITE THIS SHIT TOO
 	}
