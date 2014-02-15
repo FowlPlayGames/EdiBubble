899c2a5734428e81a33b7ec1829cd6c1678b6df5
JG24Drive45 <JG24Drive45@gmail.com>
2014-02-15 01:50:28 -0500

    new
    scripts, media files

---
+++ Assets/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+	public bool bAlive;
+	public float fSpeed;
+	public float fSize;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
