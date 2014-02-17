5bbf22583a87bb8a948c44441a7e5849f3034416
Patrick Rasmussen <mcman407@gmail.com>
2014-02-17 02:07:47 -0500

    update

--- Assets/Assets/Scripts/LevelGenerator.cs
+++ Assets/Assets/Scripts/LevelGenerator.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class LevelGenerator : MonoBehaviour 
 {
 	// public variables
-	public GameObject [] levelPieces;	// an array of level prefabs
+	public Transform horizontalPiece; 	// the defualt horizontal piece 
+	public Transform verticalPiece; 	// the defualt vertical piece
+	public Transform [] verticalTurns;     // turns that connect vertical to horizontal 
+	public Transform [] horizontalTurns; // turns that connect horizontal to vertical
 
 	//private variables
 	private PlayerScript player;		// access to player script
-	private Vector2 spawnPosition;      // marker for next spawn position
+	private Vector2 playerPosition, firstPosition, currentPosition, lastPosition;      // marker for next spawn position
 	private bool goingHorizontal, goingVertical; //bools to tell the numbe generator which numbers ar of access.
+	private int  horizontalCount, verticalCount;
 
 	// This is call first
 	void Awake()
