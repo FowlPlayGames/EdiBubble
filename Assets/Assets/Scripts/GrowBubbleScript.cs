0df1c0e0bbca5ab91d7553ff3e1531fa63cd1935
JG24Drive45 <JG24Drive45@gmail.com>
2014-02-26 04:55:23 -0500

    Grow Bubble
    grow bubble random scale
    keyboard controls for testing

---
+++ Assets/Assets/Scripts/GrowBubbleScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrowBubbleScript : MonoBehaviour {
+
+	public Vector2 scaleSize;
+	CircleCollider2D circleCollider;
+
+	// Use this for initialization
+	void Start () {
+		float scale = Random.Range( 0.5f, 3.0f );
+		scaleSize = new Vector2( scale, scale );
+		transform.localScale = scaleSize;	
+		circleCollider = gameObject.GetComponent<CircleCollider2D>();
+		circleCollider.radius = 0.2f * scale * 0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Assets/Scripts/PlayerScript.cs
+++ Assets/Assets/Scripts/PlayerScript.cs
@@ -8,6 +8,8 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 	public static PlayerScript playerScript;							// Creates an instance of the script.
+	CircleCollider2D circleCollider;
+
 	// CONSTANT VARS
 	private const float fYTiltCalibration 		= 0.3f;					// Calibration -- Might want to use first so many frames to capture this per user
 	private const float fMinSize 				= 1.0f;					// Minimum size player bubble can be
@@ -30,6 +32,8 @@ public class PlayerScript : MonoBehaviour {
 	private bool bRebounding;											// Did the player recently collide with rocks?
 	private Vector3 v3ReboundVec;										// Rebounding velocity
 
+	private bool bUsingKeyboard = true;
+
 	void Start () 
 	{
 		v3Tilt 			= Vector3.zero;									// Zero out v3Tilt vector
@@ -40,50 +44,82 @@ public class PlayerScript : MonoBehaviour {
 		fSpeed			= 100.0f;										// Set the speed of the player bubble
 	}
 
+	void Update()
+	{
+		circleCollider = gameObject.GetComponent<CircleCollider2D>();
+		circleCollider.radius = 0.04f * transform.localScale.x;
+	}
+
 	void FixedUpdate () 
 	{
-		// IF NOT ALREADY REBOUNDING FROM A COLLISION WITH A WALL
-		if( !bRebounding )												
+		// TAKE THIS IF STATEMENT OUT ONCE TESTING WITH KEYBOARD IS COMPLETE
+		if( !bUsingKeyboard )
 		{
-			v3Tilt = Input.acceleration;									// Get the tilt vector
-			v3Tilt.z = 0.0f;												// Zero out z component
+			// IF NOT ALREADY REBOUNDING FROM A COLLISION WITH A WALL
+			if( !bRebounding )												
+			{
+				v3Tilt = Input.acceleration;									// Get the tilt vector
+				v3Tilt.z = 0.0f;												// Zero out z component
 
-			v3Tilt.x = ClipTilt( v3Tilt.x );								// Check if tilt x value is too large or small
-			v3Tilt.x *= fSpeed * Time.deltaTime;							// Calculate position change in the x direction
+				v3Tilt.x = ClipTilt( v3Tilt.x );								// Check if tilt x value is too large or small
+				v3Tilt.x *= fSpeed * Time.deltaTime;							// Calculate position change in the x direction
 
-			v3Tilt.y += fYTiltCalibration;									// Calculate tilt in the y direction
-			v3Tilt.y = ClipTilt( v3Tilt.y );								// Check if tilt y value is too large or small
-			v3Tilt.y *= fSpeed * Time.deltaTime;							// Calculate position change in the y direction
+				v3Tilt.y += fYTiltCalibration;									// Calculate tilt in the y direction
+				v3Tilt.y = ClipTilt( v3Tilt.y );								// Check if tilt y value is too large or small
+				v3Tilt.y *= fSpeed * Time.deltaTime;							// Calculate position change in the y direction
 
-			transform.position += v3Tilt;									// Update the overall position
+				transform.position += v3Tilt;									// Update the overall position
+			}
+			// ELSE ALREADY REBOUNDING FROM A COLLISION WITH A WALL -- STILL RECOVERING FROM HITTING A WALL
+			else
+			{
+				transform.position += v3ReboundVec;								// Update the position by the rebound vector
+				fReboundTimer -= Time.deltaTime;								// Decrement the rebound timer
+			}
+
+			// IF THE REBOUND TIMER HAS EXPIRED
+			if( fReboundTimer <= 0.0f )										
+			{
+				fReboundTimer = fOriginalReboundTime;							// Reset the rebound timer for the next collision with a wall
+				bRebounding = false;											// Set fRebounding to false so that position can be updated via input
+			}
 		}
-		// ELSE ALREADY REBOUNDING FROM A COLLISION WITH A WALL -- STILL RECOVERING FROM HITTING A WALL
+
+
+		// FOR TESTING WITH THE KEYBOARD
 		else
 		{
-			transform.position += v3ReboundVec;								// Update the position by the rebound vector
-			fReboundTimer -= Time.deltaTime;								// Decrement the rebound timer
-		}
-
-		// IF THE REBOUND TIMER HAS EXPIRED
-		if( fReboundTimer <= 0.0f )										
-		{
-			fReboundTimer = fOriginalReboundTime;							// Reset the rebound timer for the next collision with a wall
-			bRebounding = false;											// Set fRebounding to false so that position can be updated via input
+			if( Input.GetKey( KeyCode.UpArrow ) )
+			{
+				Debug.Log( "pressed up" );
+				transform.position += new Vector3( 0.0f, 2.0f * Time.deltaTime, 0.0f );
+			}
+			if( Input.GetKey( KeyCode.DownArrow ) )
+			{
+				transform.position += new Vector3( 0.0f, -2.0f * Time.deltaTime, 0.0f );
+			}
+			if( Input.GetKey( KeyCode.LeftArrow ) )
+			{
+				transform.position += new Vector3( -2.0f * Time.deltaTime, 0.0f, 0.0f );
+			}
+			if( Input.GetKey( KeyCode.RightArrow ) )
+			{
+				transform.position += new Vector3( 2.0f * Time.deltaTime, 0.0f, 0.0f );
+			}
 		}
 	}
 
 	void OnCollisionEnter2D( Collision2D other )
 	{
+		// TODO: Make sure this bubble you are eating is smaller than you
 		// IF YOU ATE A SMALLER BUBBLE
-		if( other.gameObject.tag == "TestCube" )
+		if( other.gameObject.tag == "GrowBubble" && transform.localScale.x >= other.transform.localScale.x )
 		{
-			Debug.Log( "You hit shit!" );
+			Debug.Log( "You ate a grow bubble!" );
 
 			audio.PlayOneShot( acEatBubble );								// Play eat bubble sound
 
-			other.transform.position = 										// for testing -- move square to a random position
-				new Vector3( Random.Range( -4.0f, 4.0f ), 
-							 Random.Range( -4.0f, 4.0f ), 0.0f );		
+			Destroy( other.gameObject );									// Destroy the grow bubble	
 
 			// IF THERE IS ROOM TO GROW
 			if( fMaxGrowSize < fMaxSize )							
