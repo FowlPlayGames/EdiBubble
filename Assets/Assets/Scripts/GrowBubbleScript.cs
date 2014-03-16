056fb1833e88fedb92fe7f798ef91466fdbdb304
Jill <jg24drive45@gmail.com>
2014-03-15 22:09:48 -0400

    Grow Bubble
    Grow Bubble Shader

--- Assets/Assets/Scripts/GrowBubbleScript.cs
+++ Assets/Assets/Scripts/GrowBubbleScript.cs
@@ -6,8 +6,14 @@ public class GrowBubbleScript : MonoBehaviour {
 	public Vector2 scaleSize;
 	CircleCollider2D circleCollider;
 
+	public float greenValue = 0.0f;
+	public float blueValue = 1.0f;
+	public float colorChangeRate = 0.05f;
+	public bool greenValueIncreasing = true;
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		float scale = Random.Range( 0.5f, 3.0f );
 		scaleSize = new Vector2( scale, scale );
 		transform.localScale = scaleSize;	
@@ -16,7 +22,34 @@ public class GrowBubbleScript : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+
+		Color tintColor = GenerateTintColor( ref greenValue, ref blueValue, ref greenValueIncreasing );
+		renderer.material.SetColor( "_Color", tintColor );
+	}
+
+	public Color GenerateTintColor( ref float green, ref float blue, ref bool increasing )
+	{
+		if( increasing && green < 1.0f )
+		{
+			green += colorChangeRate;
+			blue = 1.0f - green;
+			if( green >= 1.0f )
+			{
+				increasing = false;
+			}
+		}
+		else if( !increasing && green > 0.0f )
+		{
+			green -= colorChangeRate;
+			blue = 1.0f - green;
+			if( green <= 0.0f )
+			{
+				increasing = true;
+			}
+		}
+
+		return new Color( green, green, blue, 1.0f );
 	}
 }
---
+++ Assets/Assets/Shaders/GrowBubbleShader.shader
@@ -0,0 +1,34 @@
+﻿Shader "Shaders/Test"
+{
+    Properties
+    {
+        _Color ("Color Tint", Color) = (1,1,1,1)
+        _MainTex ("Main Tex", 2D) = "white"
+    }
+ 
+    Category
+    {
+    	// Lighting Off
+    	// ZWrite Off
+    	// Cull back
+        Blend SrcAlpha OneMinusSrcAlpha
+        
+      
+               
+        Tags {Queue=Transparent}
+ 
+        SubShader
+        {
+        	Pass
+            {
+            	SetTexture [_MainTex]
+            		{
+                    	ConstantColor [_Color]
+                   		Combine Texture * constant
+                	}
+            }
+        }
+        
+     
+    }
+}
